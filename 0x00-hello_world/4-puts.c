92f7660cf88a762299eedd675bc562e510f35b6d
Davie Gathu <kinyuagathu@gmail.com>
2022-06-30 15:29:41 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
-int main(){
-	puts("Programming is like building a multilingual puzzle");
+
+/**
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Retrun: Always 0 (success)
+ */
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
 	
 	getchar();
 	
