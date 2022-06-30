76db2c0a21646de97fa5e54f84c14bd3305564b6
Davie Gathu <kinyuagathu@gmail.com>
2022-06-30 15:55:34 -0700

    changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 
 /**
- * main = Entry point
+ * main - Print size of types of computers
+ *
+ * Descriptiong: prints the size of types of computers when executed.
  *
  * Return: Always 0 (Success)
  */
@@ -9,7 +11,7 @@
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	printf("Size of an int:  %d byte(s)\n", (int) sizeof(int));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
