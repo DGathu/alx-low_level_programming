21de745c7720e7f09743d042df0f8806821f1c20
Davie Gathu <kinyuagathu@gmail.com>
2022-07-01 12:15:09 -0700

    changes

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/* more headers go there */
+/**
+ * main - prints numbers
+ *
+ * Description: prints possible combinations
+ * single digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+/*betty style doc for function main goes there */
+int main(void)
+{
+	int x;
+
+	x = 48;
+
+	while (x < 58)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
