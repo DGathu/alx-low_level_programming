ce88f83d5d40cdecab5542d0bd6fa08b6ea3fd33
Davie Gathu <kinyuagathu@gmail.com>
2022-07-01 11:43:30 -0700

    changes

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/* more headers go there */
+/**
+ * main - prints alphabets
+ *
+ * Description: prints the alphabets
+ *except q and e
+ * Return: Always 0 (Success)
+ */
+
+/*betty style doc for function main goes there */
+int main(void)
+{
+	int x = 97;
+
+	while (x < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		x++;
+	}
+	putchar(10);
+	return (0);
+}
