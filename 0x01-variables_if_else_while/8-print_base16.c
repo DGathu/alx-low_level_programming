e4f5f0e3fd8ffbe25099ab4c41b8bbecb1233d81
Davie Gathu <kinyuagathu@gmail.com>
2022-07-01 12:07:57 -0700

    changes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/* more headers go there */
+/**
+ * main - prints hexes
+ *
+ * Description: prints the hexadecimal 
+ * numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+/*betty style doc for function main goes there */
+int main(void)
+{
+	int x;
+
+	x = 0;
+
+	while (x < 48)
+	{
+		if (x < 10)
+			putchar(i + '0');
+		else if (x > 41)
+			putchar(i - 10 + 'A');
+		x++;
+	}
+	putchar(10);
+	return (0);
+}
