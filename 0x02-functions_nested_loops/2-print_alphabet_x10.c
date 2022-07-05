52fef3832100d2b40eb579d8ad0e1c0b8638a446
Davie Gathu <kinyuagathu@gmail.com>
2022-07-05 11:13:28 -0700

    changes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+  
+/**
+ * print_alphabet_x10 - prints alphabetx10
+ *
+ * Description: prints alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int times = 0;
+	char alpha = 'a';
+
+	while (times < 10)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+
+		times++;
+	}
+}
