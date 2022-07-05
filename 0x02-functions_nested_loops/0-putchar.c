e0358e26ef22b6676fb9105ab18707f2ac20fcda
Davie Gathu <kinyuagathu@gmail.com>
2022-07-05 10:33:53 -0700

    changes

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char text[10] = "_putchar";
+	int x = 0;
+
+	for (x=0; x < 8; x++)
+	{
+		_putchar(text[x]);
+	}
+	_putchar('\n');
+	return (0);
+}
