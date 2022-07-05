31d905873ebd7dd512039050ae26dc394118506d
Davie Gathu <kinyuagathu@gmail.com>
2022-07-05 15:05:50 -0700

    changes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @n1: n1 is integer 1
+ * @n2: n2 is integer 2
+ *
+ * Return: sum
+ */
+
+int add(int, int)
+{
+	int n1, n2, sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
