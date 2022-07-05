e48ca92128ea871d82be386f4a04b9f489296cd9
Davie Gathu <kinyuagathu@gmail.com>
2022-07-05 11:48:12 -0700

    changes

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  _isalpha - checks alpabet isalpha
+ *
+ * @c: alphas in ascii
+ *
+ * Return: 1 if alpha
+ */
+
+int _isalpha(int c)
+{
+	if (c >= A && c <= Z)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
