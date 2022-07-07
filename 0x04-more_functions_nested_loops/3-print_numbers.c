c0735fc14997b10ace1e8c6d0cabd4292f3e660c
Davie Gathu <kinyuagathu@gmail.com>
2022-07-07 14:23:30 -0700

    changes

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9
+ * 
+ * Description: prints 0-9
+ * 
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int x;
+	while (x = '0'; x < '10'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
