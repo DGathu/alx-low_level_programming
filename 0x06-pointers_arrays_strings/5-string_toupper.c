83fd7699867e39cd72bfdb1116f6325eb18c72e4
Davie Gathu <kinyuagathu@gmail.com>
2022-07-14 03:27:46 -0700

    changes

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - changes to upper
+ *
+ * Return: String
+ */
+
+char *string_toupper(char *)
+{
+	int x = 0;
+	char c;
+
+	for (x = 0; c[x] != '\0'; x++)
+	{
+		if (c[x] >= 'a' && c[x] <= 'z')
+		{
+			c[x] = c[x] - 'a' + 'A';
+		}
+	}
+	return (c);
+}
