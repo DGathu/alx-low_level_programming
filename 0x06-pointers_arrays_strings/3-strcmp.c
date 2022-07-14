d2c2f303101ff24a6695da0d07808d874e091c3c
Davie Gathu <kinyuagathu@gmail.com>
2022-07-14 03:02:15 -0700

    changes

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,16 +13,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int x = 0;
 
-	while(s1[x] == s2[x])
+	while (*(s1 + x)  ==  *(s2 + x)  && *(s1 + x))
 	{
-		if (s1[x] == '\0' || s2[x] == '\0')
-		{
-			break;
-		}
 		x++;
 	}
-	if (s1[x] == '\0' && s2[x] == '\0')
+	if (*(s2 + x))
 	{
-		return 0;
+		return (*(s1 + x) - *(s2 + x));
 	}
+	else
+	{
+		return (0);
 }
