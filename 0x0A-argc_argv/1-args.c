37aea49ddf4df696e342c31baba1b0378628b6d2
Davie Gathu <kinyuagathu@gmail.com>
2022-07-22 16:40:57 -0700

    add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints name of program
+ * @argc: no of arguments
+ * @argv: array of arg
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] _attribute_((unused)))
+{
+	if (argc > 0)
+	{
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
